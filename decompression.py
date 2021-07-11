41cc849e073bcb4f2d06de416f8b9cc1af1b7f1a
DP_OP <deepakpattnayak2013@gmail.com>
2021-07-11 14:02:08 +0530

    update update

--- decompression.py
+++ decompression.py
@@ -1,6 +1,4 @@
 from node import Node
-import array
-
 
 class HuffmanDeCompressor:
     def __init__(self):
@@ -19,11 +17,9 @@ class HuffmanDeCompressor:
         inputFile = open(inputFileName, "rb")
         encodedBytes = ""
         byte = inputFile.read(1)
-
         while len(byte) > 0:
             encodedBytes += f"{bin(ord(byte))[2:]:0>8}"
             byte = inputFile.read(1)
-
         return encodedBytes
 
     def decode(self, encodedBytes: str):
@@ -34,33 +30,26 @@ class HuffmanDeCompressor:
         encodedBytes = ''.join(bitsStream)
         outputBytes = []
         byteKey = ''
-
         for bit in encodedBytes:
             byteKey += bit
             byteValue = reversedLookupTable.get(byteKey)
-
             if byteValue is not None:
                 outputBytes.append(byteValue)
                 byteKey = ''
-
         return outputBytes
 
     def decodeTree(self, bitsStream):
-
         bit = bitsStream[0]
         del bitsStream[0]
-
         if bit == "1":
             byte = ""
             for _ in range(8):
                 byte += bitsStream[0]
                 del bitsStream[0]
-
             return Node(int(byte, 2))
         else:
             left = self.decodeTree(bitsStream)
             right = self.decodeTree(bitsStream)
-
             return Node(None, left=left, right=right)
 
     def removePadding(self, bitsStream):
@@ -68,18 +57,14 @@ class HuffmanDeCompressor:
         numOfZeros_Int = int("".join(numOfZeros_Bin), 2)
         bitsStream = bitsStream[8:]
         bitsStream = bitsStream[numOfZeros_Int:]
-
         return bitsStream
 
     def buildReversedLookupTable(self, huffmanTree: Node):
         lookupTable = {}
-
         self.buildReversedLookupTableImpl(huffmanTree, "", lookupTable)
-
         if len(lookupTable) == 1:
             key = next(iter(lookupTable))
             lookupTable[key] = '1'
-
         return {v: k for k, v in lookupTable.items()}
 
     def buildReversedLookupTableImpl(self, node: Node, code, lookupTable):
@@ -91,14 +76,10 @@ class HuffmanDeCompressor:
 
     def save(self, outputFileName: str, outputBytesNum):
         outputBytes = ''
-
         for num in outputBytesNum:
             outputBytes += format(num, '08b')
-
         b_arr = bytearray()
-
         for i in range(0, len(outputBytes), 8):
             b_arr.append(int(outputBytes[i:i + 8], 2))
-
         outputFile = open(f"OG{outputFileName}", "wb")
         outputFile.write(b_arr)
