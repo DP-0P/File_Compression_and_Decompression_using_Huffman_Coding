0a33aacc669721a0624e47b0cf0f782d25678f0f
DP_OP <deepakpattnayak2013@gmail.com>
2021-07-11 14:49:36 +0530

    update update

--- main.py
+++ main.py
@@ -4,27 +4,20 @@ import sys
 from decompression import HuffmanDeCompressor
 from compression import HuffmanCompressor
 
-
 def main():
     value = input('(0) To Exit:\n(1) Compress File:\n(2) Decompress File:\n')
-
     startTime = time.time()
-
     if value == '1':
         encoder = HuffmanCompressor()
         name = input('Enter Location: ')
         encoder.compressFile(name)
-
     elif value == '2':
         decompressor = HuffmanDeCompressor()
         name = input('Enter Location: ')
         decompressor.decompressFile(name)
-
     elif value == '0':
         sys.exit("Program Terminated...")
-
     print("Time Elapsed: %s seconds" % (time.time() - startTime)+"\n")
-
 
 while True:
     main()
